6e6fa74d2ba8fba81395cda75fb7ba0ac64a9129
son <ourendpoint@gmail.com>
2021-06-23 21:45:17 +0900

    feat: 드래그 기본 기능 구현 및 모델 설계

--- src/js/index.js
+++ src/js/index.js
@@ -28,24 +28,38 @@ const fileInput = document.getElementById('image_input')
 fileInput.onchange = setThumbnail
 
 /** init */
+// TODO: isReady가 true면 정답에서 부터 거꾸로 맞추기
+// TODO: isReady가 false면 섞지 말기
+// TODO: isStarted가 true 면 정답 맞출 경우 정답처리. false면 섞는 중으로 간주되므로 정답처리 X
 let move = false
-let selected = NaN
+let selected = 0
+let beforeCursorX = 0
+let beforeCursorY = 0
 document.body.addEventListener('mousedown', () => {
   move = true
-  console.log('move: true')
 })
 document.body.addEventListener('mouseup', () => {
   move = false
-  console.log('move: false')
+  selected = 0
 })
 const indexes = Array.apply(0, new Array(17)).map((_, idx) => idx + 1)
 indexes.map((index) => {
   const piece = document.getElementById(`piece${index}`)
-  piece.addEventListener('mousedown', () => {
+  // TODO: canvas 16등분으로 잘라서 각 piece 안에 삽입
+  // TODO: blank공간 예외처리
+  piece.addEventListener('mousedown', (event) => {
     selected = index
+    beforeCursorX = event.pageX
+    beforeCursorY = event.pageY
   })
-  piece.addEventListener('mousemove', () => {
-    move ? console.log(index) : null
+  piece.addEventListener('mousemove', (event) => {
+    if(move && selected) {
+      const selectedPiece = document.getElementById(`piece${selected}`)
+      // TODO: translateX, Y가 block의 width, height를 넘길 경우 maximum으로 fix
+      selectedPiece.style.transform = `translate(${event.pageX - beforeCursorX}px, ${event.pageY - beforeCursorY}px)`
+      // TODO: 만약 일정 translateX가 넘어가면 translateX horizental canvas swap and fix
+      // TODO: 만약 일정 translateY가 넘어가면 translateY vertical canvas swap and fix
+    }
   })
 })
 document.getElementById('')
